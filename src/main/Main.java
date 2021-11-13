ded8eac8021957b5ed4083cd5ffc3c1d5725c3da
ACLAVERIA <andre.claveria@ynov.com>
2021-11-13 16:58:16 +0100

    Board pas encore finis

--- src/main/Main.java
+++ src/main/Main.java
@@ -28,87 +28,29 @@ public class Main {
 	}
 	
 	public static void fillTab(String[][] tableauDeDame) {
-				tableauDeDame[0][0] = "     ";
-				tableauDeDame[0][1] = "  -  ";
-				tableauDeDame[0][2] = "     ";
-				tableauDeDame[0][3] = "  -  ";
-				tableauDeDame[0][4] = "     ";
-				tableauDeDame[0][5] = "  -  ";
-				tableauDeDame[0][6] = "     ";
-				tableauDeDame[0][7] = "  -  ";
-				
-				tableauDeDame[1][0] = "  -  ";
-				tableauDeDame[1][1] = "     ";
-				tableauDeDame[1][2] = "  -  ";
-				tableauDeDame[1][3] = "     ";
-				tableauDeDame[1][4] = "  -  ";
-				tableauDeDame[1][5] = "     ";
-				tableauDeDame[1][6] = "  -  ";
-				tableauDeDame[1][7] = "     ";
-				
-				tableauDeDame[2][0] = "     ";
-				tableauDeDame[2][1] = "  -  ";
-				tableauDeDame[2][2] = "     ";
-				tableauDeDame[2][3] = "  -  ";
-				tableauDeDame[2][4] = "     ";
-				tableauDeDame[2][5] = "  -  ";
-				tableauDeDame[2][6] = "     ";
-				tableauDeDame[2][7] = "  -  ";
-				
-				tableauDeDame[3][0] = "  -  ";
-			
-				tableauDeDame[3][1] = "     ";
-				tableauDeDame[3][2] = "  -  ";
-				tableauDeDame[3][3] = "     ";
-				tableauDeDame[3][4] = "  -  ";
-				tableauDeDame[3][5] = "     ";
-				tableauDeDame[3][6] = "  -  ";
-				tableauDeDame[3][7] = "     ";
-				
-				tableauDeDame[4][0] = "     ";
-			
-				tableauDeDame[4][1] = "  -  ";
-				tableauDeDame[4][2] = "     ";
-				tableauDeDame[4][3] = "  -  ";
-				tableauDeDame[4][4] = "     ";
-				tableauDeDame[4][5] = "  -  ";
-				tableauDeDame[4][6] = "     ";
-				tableauDeDame[4][7] = "  -  ";
-				
-				tableauDeDame[5][0] = "  -  ";
-		
-				tableauDeDame[5][1] = "     ";
-				tableauDeDame[5][2] = "  -  ";
-				tableauDeDame[5][3] = "     ";
-				tableauDeDame[5][4] = "  -  ";
-				tableauDeDame[5][5] = "     ";
-				tableauDeDame[5][6] = "  -  ";
-				tableauDeDame[5][7] = "     ";
-				
-				tableauDeDame[6][0] = "     ";
-	
-				tableauDeDame[6][1] = "  -  ";
-				tableauDeDame[6][2] = "     ";
-				tableauDeDame[6][3] = "  -  ";
-				tableauDeDame[6][4] = "     ";
-				tableauDeDame[6][5] = "  -  ";
-				tableauDeDame[6][6] = "     ";
-				tableauDeDame[6][7] = "  -  ";
-			
-				tableauDeDame[7][0] = "  -  ";
-	
-				tableauDeDame[7][1] = "     ";
-				tableauDeDame[7][2] = "  -  ";
-				tableauDeDame[7][3] = "     ";
-				tableauDeDame[7][4] = "  -  ";
-				tableauDeDame[7][5] = "     ";
-				tableauDeDame[7][6] = "  -  ";
-				tableauDeDame[7][7] = "     ";
-		
-			
+		for(int i = 0 ; i < tableauDeDame.length ; i++) {
+			if(i%2 == 0) {
+				for(int j = 0 ; j < tableauDeDame[i].length ; j++) {
+					if(j%2 == 0) {
+						tableauDeDame[i][j] = "     ";
+					} else {
+						tableauDeDame[i][j] = "  -  ";
+					}
+				}
+			} else {
+				for(int j = 0 ; j < tableauDeDame[i].length ; j++) {
+					if(j%2 != 0) {
+						tableauDeDame[i][j] = "     ";
+					} else {
+						tableauDeDame[i][j] = "  -  ";
+					}	
+				}
+			}	
+		}
+
 		printTab(tableauDeDame);
 	}
-		
+			
 	
 	public static void DrawHorizontalLine() {        
     System.out.println(" _______________________________________________");   
